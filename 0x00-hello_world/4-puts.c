85c2c97ee06a359d0683940de62b05d37b68682f
Bakkabulindi <ebakka4@gmail.com>
2022-12-22 17:32:23 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * main - Entry
  * Return 0 all the time
  */
 int main(void)
-	puts("\"Programming is like building a multilingual puzzle");
+(
+ puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
+	)
